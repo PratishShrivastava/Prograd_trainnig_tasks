0a992d4e8dfc70a926b7449928a08ed4bd79699a
PratishShrivastava <pratishshrivastava395@gmail.com>
2022-07-21 16:38:00 +0530

    Updated Task Again

--- Q37_TransposeMatrix.java
+++ Q37_TransposeMatrix.java
@@ -23,6 +23,14 @@ public class Q37_TransposeMatrix {
             }
         }
 
+        System.out.println("The above matrix before Transpose is ");
+        for(i = 0; i < row; i++){
+            for(j = 0; j < column; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
         System.out.println("The above matrix after Transpose is ");
         for(i = 0; i < column; i++) {
             for(j = 0; j < row; j++)
